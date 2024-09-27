56daf7bb27416f74bb0418f31267e5d1c359c2eb
Dinesh97898 <167900538+Dinesh97898@users.noreply.github.com>
2024-09-27 20:31:36 +0530

    get time tests sucessfull

--- exercise_3/exercise_3.py
+++ exercise_3/exercise_3.py
@@ -2,3 +2,12 @@ import json
 class Exercise3:
     def __init__(self,json_file):
         self.json_data = json.load(open(json_file,'r'))
+
+    def get_username(self):
+        return self.json_data['username']
+    
+    def get_time_remaining(self):
+        return self.json_data['time_remaining']
+    
+    def add_hour(self):
+        self.json_data['time_remaining'] += 60

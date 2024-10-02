76500ad1092fdf8a4af16fcdce203a5332c203f5
Dinesh97898 <167900538+Dinesh97898@users.noreply.github.com>
2024-10-02 18:48:56 +0530

    Added code for all test cases

--- exercise_4/triangle.py
+++ exercise_4/triangle.py
@@ -1,1 +1,22 @@
+class Triangle:
+
+    #Constructor
+    def __init__(self,base,height) -> None:
+        self.height = height
+        self.base = base
+
+    # Repr function
+    def __repr__(self) -> str:
+        return f"Triangle: base={self.base}, height={self.height}"
+    
+    #calculate Hypoteuse
+    def hypotenuse(self):
+        return (self.base**2 + self.height**2)**0.5
+    
+    #calculate area
+    def area(self):
+        return (1/2)*self.height*self.base
+    
+    
+
 
